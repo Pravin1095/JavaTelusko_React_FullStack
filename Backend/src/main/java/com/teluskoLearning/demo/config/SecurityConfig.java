1a026559a36c0325577e17ea7f1ae0760730cfe8
Pravin A <a.pravin3210@gmail.com>
2026-09-05 18:03:47 +0530

    324 Custom Login

--- Backend/src/main/java/com/teluskoLearning/demo/config/SecurityConfig.java
+++ Backend/src/main/java/com/teluskoLearning/demo/config/SecurityConfig.java
@@ -7,9 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -41,10 +43,15 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http.csrf(customizer -> customizer.disable())
-                .authorizeHttpRequests(request -> request.requestMatchers("/register").permitAll().anyRequest().authenticated()).
+                .authorizeHttpRequests(request -> request.requestMatchers("/register", "/login").permitAll().anyRequest().authenticated()).
                 sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationmanager(AuthenticationConfiguration config){
+return config.getAuthenticationManager(); //We utilize this AuthenticationManager object inside login route
     }
 
 
--- Backend/src/main/java/com/teluskoLearning/demo/controller/UserController.java
+++ Backend/src/main/java/com/teluskoLearning/demo/controller/UserController.java
@@ -1,6 +1,9 @@
 package com.teluskoLearning.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,10 +15,24 @@ import com.teluskoLearning.demo.service.UserService;
 public class UserController {
 
     @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
     private UserService service;
 
     @PostMapping("register")
     public User register(@RequestBody User user) {
         return service.saveUser(user);
+    }
+
+    @PostMapping("login")
+    public String login(@RequestBody User user){
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+        if(authentication.isAuthenticated()){
+            return "Success";
+        }
+        else{
+            return "Login Failed";
+        }
     }
 }
